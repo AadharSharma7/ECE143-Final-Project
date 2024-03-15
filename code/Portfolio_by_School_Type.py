2dbee98e50e463fa099b64dd168b40ee4c5b425a
achyuthem <32310714+achyuthem@users.noreply.github.com>
2024-03-15 16:30:43 -0700

    Update Portfolio_by_School_Type.py

--- code/Portfolio_by_School_Type.py
+++ code/Portfolio_by_School_Type.py
@@ -1,7 +1,7 @@
 # %%
 import pandas as pd
 import matplotlib.pyplot as mp
-df = pd.read_excel('../data/Portfolio_by_School_Type.xls')
+df = pd.read_excel('../data/Portfolio_by_School_type.xls')
 df
 
 # %% Drop the columns that have NA values
